0811d98a8a01d57be34070011ca138b2bffc459b
matiasmdlp <matiasmdlp95@gmail.com>
2022-09-29 14:00:02 -0300

    clases

---
+++ Tarea1_v2/src/tarea1_v2/Cliente.java
@@ -0,0 +1,13 @@
+package tarea1_v2;
+
+public class Cliente {
+    private String nombre;
+    private String rut;
+    
+    public Cliente(String n, String r){
+        nombre = n;
+        rut = r;
+    }
+}
+
+   
---
+++ Tarea1_v2/src/tarea1_v2/Direccion.java
@@ -0,0 +1,9 @@
+package tarea1_v2;
+
+public class Direccion {
+    private String direccion;
+    
+    public Direccion(String d){
+        direccion = d;
+    }
+}
